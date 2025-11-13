74d2bf662968c3320bd26fdbbb67a4ee26b7e276
Amine ACHOUHAM <amineachouhame51@gmail.com>
2025-11-13 14:10:08 +0100

    Add xml reader

--- json-loader.py
+++ json-loader.py
@@ -1,8 +1,10 @@
-from utils import FOLDER_PATH, read_all_file_name_from_folder, load_file, deep_smart_flatten
+from utils import FOLDER_PATH, read_all_file_name_from_folder, load_file, deep_smart_flatten_json
 data_flow_paths=read_all_file_name_from_folder(FOLDER_PATH)
 for path in data_flow_paths: 
-    df = load_file(path)
-    file_extension =path.split(".")[-1] 
+    df=load_file(path)
+    file_extension=path.split(".")[-1].lower() 
+    result=None
     if(file_extension=="json"):   
-       result = deep_smart_flatten(df)
-       print(result)
+       result = deep_smart_flatten_json(df)
+    elif(file_extension in ["csv", "xml"]): 
+       result = df
--- utils.py
+++ utils.py
@@ -5,12 +5,16 @@ import xml.etree.ElementTree as ET
 FOLDER_PATH = "./data/"
 def read_all_file_name_from_folder(folder_path):
     all_files = []
-
     for root, dirs, files in os.walk(folder_path):
         for file in files:
             path = os.path.join(root, file)
             all_files.append(path)
     return all_files 
+def walk_in_order(elem, level=0):
+    print("  " * level + elem.tag + ": " + (elem.text.strip() if elem.text else ""))
+    for child in elem: 
+        walk_in_order(child, level + 1)
+
 def load_file(path):
     if path.endswith(".csv"):
         return pd.read_csv(path)
@@ -23,16 +27,12 @@ def load_file(path):
     elif path.endswith(".xml"):
         tree = ET.parse(path)
         root = tree.getroot()
-        rows = []
-        for child in root:
-            row = {elem.tag: elem.text for elem in child}
-            rows.append(row)
-        return pd.DataFrame(rows)
+        return deep_smart_flatten_xml(root)
     else:
         raise ValueError("Unsupported file")
 m={}
 ind=0
-def help_deep_smart_flatten(data, new_key):
+def help_deep_smart_flatten_json(data, new_key):
     global m
     global ind
     if(data is None):
@@ -45,7 +45,7 @@ def help_deep_smart_flatten(data, new_key):
     _type = type(data)
     if(isinstance(data, list)):
        for index, value in enumerate(data):
-           help_deep_smart_flatten(value, new_key+"###"+str(index))
+           help_deep_smart_flatten_json(value, new_key+"###"+str(index))
        return 
     if(isinstance(data, int) or 
        isinstance(data, float) or 
@@ -59,27 +59,58 @@ def help_deep_smart_flatten(data, new_key):
        m[new_key].append(data)
        return
     for key in data:
-        help_deep_smart_flatten(data[key], new_key+"###"+str(key))
-def deep_smart_flatten(df):
+        help_deep_smart_flatten_json(data[key], new_key+"###"+str(key))
+def deep_smart_flatten_json(df):
     global m  
     global ind
-    global mx
     m.clear()
     ind=0
     for index, row in df.iterrows():
         for col in df.columns:
-            help_deep_smart_flatten(row[col], col)
-        ind+=1
+            help_deep_smart_flatten_json(row[col], col)   
+        ind+=1    
     for key in m:
        while(len(m[key])<ind):
-           m[key].append(None) 
-       
+           m[key].append(None)    
     result=[{} for _ in range(ind)]
     for new_ind in range(ind):
         for key in m:
             new_key = key.split("###")
             if(len(new_key)==0):
-                continue
+               continue
             new_key = '_'.join(new_key[-2:])  
             result[new_ind][new_key]=m[key][new_ind]
-    return pd.json_normalize(result)          
+    return pd.json_normalize(result)  
+def help_deep_smart_flatten_xml(root, new_key):
+    global ind
+    if(root.text is None or root.text.strip()!=""):
+       if(new_key not in m):
+          m[new_key]=[]
+       while(len(m[new_key])<ind):
+          m[new_key].append(None)
+       if(root.text is None):
+          m[new_key].append(None)
+       else:  
+          m[new_key].append(root.text.strip())          
+    for child in root:        
+        help_deep_smart_flatten_xml(child, new_key+"###"+child.tag)      
+def deep_smart_flatten_xml(root):
+    global m  
+    global ind
+    ind=0
+    m.clear()        
+    for child in root:
+        help_deep_smart_flatten_xml(child, child.tag)
+        ind+=1 
+    for key in m: 
+       while(len(m[key])<ind):
+          m[key].append(None) 
+    result=[{} for _ in range(ind)]
+    for new_ind in range(ind):
+        for key in m:
+            new_key = key.split("###")
+            if(len(new_key)==0):
+               continue
+            new_key = '_'.join(new_key[-2:])  
+            result[new_ind][new_key]=m[key][new_ind]
+    return pd.json_normalize(result)  
